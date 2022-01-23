5dc2df13d72d8b690edab3583cfa8b826dc7859f
Tobias S. Keller <tobias.s.keller@gmail.com>
2022-01-23 02:16:38 +0100

    Startup Script

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ export CONFIGLOCATION="$(pwd)/config.yaml"
 export SETTINGS_PATH="$(pwd)/config"
 export SHOWDATA_PATH="$(pwd)/config/showfiles"
 # export DEBUG=obs-websocket-js:*
-python3 serialproxy.py
+python3 serialproxy.py&
 (cd app && meteor)
